a15f5db7f9d356dff64c3bee667acef92465f00a
Maxime Warnier <marmax@gmail.com>
2015-07-21 21:49:44 +0200

    better CLI for autoinstalling plugin

---
+++ src/handlePluginHelper.js
@@ -0,0 +1,111 @@
+"use strict";
+//const npm = require("npm");
+
+var exec = require("child_process").exec;
+
+const inquirer = require("inquirer");
+const clor = require("clor");
+
+
+
+module.exports = handlePlugin;
+
+//TODO help message if plugin not found
+// -> Auto install
+// TODO provide silent option
+function handlePlugin(plugin, type){
+  //TODO test if valide object > require
+  //var plugin;
+  var promisePlugin;
+
+  var pkgName = "codemetrics-"+type+"-"+plugin.name ;
+
+
+  try {
+    promisePlugin = Promise.resolve(require(pkgName)(plugin.options));
+  } catch(e){
+    // TODO Check if error is only for the package that codemetrics wants to require
+    if ( e.code === "MODULE_NOT_FOUND" ) {
+      let msg = clor.yellow(`The ${plugin.name} ${type} ( ${pkgName} ) cannot be found.`)
+      .line("You have to install it before running CodeMetrics.")
+      .line
+      .line.bold.yellow("May I try to do it for you ?")
+      .line();
+
+      log(msg.toString()) ;
+
+      promisePlugin = askForAutoInstall(pkgName)
+             .then(() => {
+                log(pkgName+ " install in progress...","green");
+                return installPackage(pkgName);
+              })
+             .then(() => handlePlugin(plugin,type))
+      //.then((plugin) => resolve(plugin))
+      //.catch((message) => reject(message)
+
+    } else {
+      promisePlugin = Promise.reject({
+        msg : "Plugin initialisation error",
+        errorMsg : e
+      });
+    }
+  }
+
+  return promisePlugin;
+}
+
+function askForAutoInstall(pkgName){
+
+  //clor.red.bold("fee").line.inverse("fi").line.underline("fo")
+
+  return new Promise(function(resolve,reject) {
+    inquirer.prompt([{
+    type: "confirm",
+    name: "autoinstall",
+    message: "Do you want to install "+ pkgName +" ?",
+    default:true,
+  }], function(response) {
+    if(response.autoinstall) {
+      resolve();
+    } else {
+    reject({
+      msg : "Sorry I can't continue. You should provide an existing plugin..."
+    });
+  }
+  });
+
+  });
+}
+
+/* Require npm dependeny
+function installPackage(pkgName) {
+npm.load(null,function(err, _npm) {
+  if(err){
+    console.log(err);
+  }
+
+  _npm.commands.install([pkgName],function(){
+    hello = require(pkgName);
+    cb();
+  });
+});
+}
+*/
+
+function installPackage(pkgName) {
+  return new Promise(function(resolve,reject){
+    exec("npm install "+pkgName,function(error, stdout, stderr) {
+        if (error || stderr) {
+          reject(error,stderr);
+        } else {
+          resolve(stdout);
+        }
+    });
+  });
+}
+
+//TODO extract to an helper
+function log(message,color){
+  message = color ? clor[color](message).toString() : message;
+  console.log(message);
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+/* global process */
 "use strict";
 
 const path = require("path");
 const cli = require("commander");
+const clor = require("clor");
+
 const Codemetrics = require("./codemetrics.js");
+const handlePluginHelper = require("./handlePluginHelper.js");
 
 
 const defaultConfig = {
@@ -32,49 +36,90 @@ cli
 
 
 const config = processConfig(cli.config);
-const verbose = cli.verbose;
+const verbose = cli.verbose ? cli.verbose : true;
 const input = cli.args[0] ;
 //console.log(config,cli.args);
 
 
+var parsers,processors,reporters;
+
+
 //TODO provide ability to handle multiples parsers
-const parsers = handlePlugin(config.parsers[0],"parser");
+//TODO parsers could be piped
+handlePlugins(config.parsers,"parser",{})
+  //.then((result) => {parsers = result})
+  .then((plugins) => handlePlugin(config.processors,"processor",plugins))
+  //.then((result) => {processors = result})
+  .then((plugins) => handlePlugin(config.reporters,"reporter",plugins))
+  //.then((result) => {reporters = result})
+  .then((plugins) => console.log("=>",plugins))
+  //TODO check input
+  /*new Codemetrics(input,verbose)
+  .parse(parsers)
+  .process(processors)
+  .report(reporters) ;
+
+/*
 const processors = config.processors.map(processor => handlePlugin(processor,"process"));
 const reporters= config.reporters.map(reporter => handlePlugin(reporter,"reporter"));
 
-
-//TODO check input
-new Codemetrics(input,verbose)
-.parse(parsers)
-.process(processors)
-.report(reporters) ;
-
-
-//TODO help message if plugin not found
-// -> Auto install
-function handlePlugin(plugin, type){
-  //TODO test if valide object > require
-  return require("codemetrics-"+type+"-"+plugin.name)(plugin.options);
+*/
+function handlePlugins(configListePlugins,type, plugins){
+  return Promise.all(configListePlugins.map(plugin=>handlePluginHelper(plugin,type)))
+  .then((result) => {
+    console.log(result);
+    plugins[type] = result;
+    return plugins;
+  })
+  .catch( (result = {msg,errorMsg} ) => {
+    log(result.msg,"red");
+    if(result.errorMsg){
+      log(clor.line.bold.red("Error : ").line(result.errorMsg).toString());
+    }
+    process.exit(1);
+  });
 }
+
+
+
+
+/*
+if(!program.args.length) {
+    program.help();
+} else {
+    console.log('Keywords: ' + program.args);
+}
+*/
+
+
+
 
 
 function processConfig(CLIconfig){
   return CLIconfig ? require(path.resolve(CLIconfig)) : loadDefaultConfig() ;
 }
 
+
 function loadDefaultConfig(){
-  var config = defaultConfig ;
+  var config  ;
   try {
     config = require(path.resolve(defaultConfigFile));
-    log("Using configuration file") ;
+    //TODO valid configuration
+    log("Using configuration file","blue") ;
   } catch(e) {
-    log("No configuration provided") ;
+    log("No configuration provided","red",true) ;
+
+    process.exit(1);
   }
   return config;
 }
 
-function log(message){
-  if(verbose){
+
+
+
+function log(message,color,bypassVerbose){
+  if(verbose || bypassVerbose){
+    message = color ? clor[color](message).toString() : message;
     console.log(message);
   }
 }
