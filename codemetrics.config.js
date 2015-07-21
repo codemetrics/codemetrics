c920ebaa229246cd85c9fddbe330c6512ad5ef86
Maxime Warnier <marmax@gmail.com>
2015-07-21 21:49:01 +0200

    sample config file

---
+++ codemetrics.config.js
@@ -0,0 +1,13 @@
+const defaultConfig = {
+    parsers : [{
+      name:"file"
+    }],
+    processors : [{
+      name:"sloc"
+    }],
+    reporters : [{
+      name:"console"
+    }]
+};
+
+module.exports = defaultConfig ;
