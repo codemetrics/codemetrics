2a211cc32664d2907908be16eb1edb626560a2ae
Maxime Warnier <marmax@gmail.com>
2015-10-16 14:12:40 +0200

    style for good linting

--- src/handlePluginHelper.js
+++ src/handlePluginHelper.js
@@ -15,86 +15,86 @@ module.exports = handlePlugin;
 // TODO provide silent option
 //
 //TODO Test si url
-function handlePlugin(plugin, type){
-    if(plugin.worker && typeof(plugin.worker) === "function") {
-        let worker = plugin.worker(plugin.options);
+function handlePlugin(plugin, type) {
+  if (plugin.worker && typeof(plugin.worker) === "function") {
+    let worker = plugin.worker(plugin.options);
 
-        if(worker.run && typeof(worker.run) === "function") {
-            return Promise.resolve(worker);
-        } else {
-            return searchForAPackage(plugin,type);
-        }
-
+    if (worker.run && typeof(worker.run) === "function") {
+      return Promise.resolve(worker);
     } else {
-        return searchForAPackage(plugin,type);
+      return searchForAPackage(plugin, type);
     }
+
+  } else {
+    return searchForAPackage(plugin, type);
+  }
 }
 
-function searchForAPackage(plugin, type){
-    var promisePlugin;
+function searchForAPackage(plugin, type) {
+  var promisePlugin;
 
-    var pkgName = "codemetrics-"+type+"-"+plugin.name ;
+  var pkgName = "codemetrics-" + type + "-" + plugin.name;
 
-    try {
-        let worker = require(pkgName)(plugin.options);
+  try {
+    const worker = require(pkgName)(plugin.options);
 
-        worker.name = worker.name || plugin.name;
+    worker.name = worker.name || plugin.name;
 
-        promisePlugin = Promise.resolve(worker);
+    promisePlugin = Promise.resolve(worker);
 
-    } catch(e){
+  } catch (e) {
     // TODO Check if error is only for the package that codemetrics wants to require
-        if ( e.code === "MODULE_NOT_FOUND" ) {
-          let msg = clor.yellow(`The ${plugin.name} ${type} ( ${pkgName} ) cannot be found.`)
-      .line("You have to install it before running CodeMetrics.")
-      .line
-      .line.bold.yellow("May I try to do it for you ?")
-      .line();
+    if (e.code === "MODULE_NOT_FOUND") {
+      const msg = clor.yellow(`The ${plugin.name} ${type} ( ${pkgName} ) cannot be found.`)
+        .line("You have to install it before running CodeMetrics.")
+        .line
+        .line.bold.yellow("May I try to do it for you ?")
+        .line();
 
-          log(msg.toString()) ;
+      log(msg.toString());
 
-          promisePlugin = askForAutoInstall(pkgName)
-             .then(() => {
-                 log(pkgName+ " install in progress...","green");
-                 return installPackage(pkgName);
-             })
-             .then(() => handlePlugin(plugin,type));
+      promisePlugin = askForAutoInstall(pkgName)
+        .then(() => {
+          log(pkgName + " install in progress...", "green");
+          return installPackage(pkgName);
+        })
+        .then(() => handlePlugin(plugin, type));
       //.then((plugin) => resolve(plugin))
       //.catch((message) => reject(message)
 
-      } else {
-          promisePlugin = Promise.reject({
-              msg : "Plugin initialisation error",
-              errorMsg : e
-          });
-      }
+    } else {
+      promisePlugin = Promise.reject({
+        msg: "Plugin initialisation error",
+        errorMsg: e
+      });
     }
+  }
 
-    return promisePlugin;
+  return promisePlugin;
 }
 
 
-function askForAutoInstall(pkgName){
+function askForAutoInstall(pkgName) {
 
   //clor.red.bold("fee").line.inverse("fi").line.underline("fo")
 
-    return new Promise(function(resolve,reject) {
-        inquirer.prompt([{
-            type: "confirm",
-            name: "autoinstall",
-            message: "Do you want to install "+ pkgName +" ?",
-            default:true,
-        }], function(response) {
-          if(response.autoinstall) {
-            resolve();
-        } else {
-            reject({
-              msg : "Sorry I can't continue. You should provide an existing plugin..."
-          });
-        }
-      });
-
+  return new Promise(function(resolve, reject) {
+    inquirer.prompt([{
+      type: "confirm",
+      name: "autoinstall",
+      message: "Do you want to install " + pkgName + " ?",
+      default: true,
+    }], function(response) {
+      if (response.autoinstall) {
+        resolve();
+      } else {
+        reject({
+          msg: "Sorry I can't continue. You should provide an existing plugin..."
+        });
+      }
     });
+
+  });
 }
 
 /* Require npm dependeny
@@ -113,30 +113,31 @@ npm.load(null,function(err, _npm) {
 */
 
 function installPackage(pkgName) {
-    return new Promise(function(resolve,reject){
-        log2("Trying to install from npm :  npm install "+pkgName);
-        exec("npm install "+pkgName,function(error, stdout, stderr) {
+  return new Promise(function(resolve, reject) {
+    log2("Trying to install from npm :  npm install " + pkgName);
+    exec("npm install " + pkgName, function(error, stdout, stderr) {
 
-          //TODO handle code error from npm
-            if (error) {
-              reject({
-                msg : error
-            });
-          } else {
-              log2(stdout);
-              resolve(stdout);
-          }
+      //TODO handle code error from npm
+      if (error) {
+        reject({
+          msg: error
         });
+      } else {
+        log2(stdout);
+        resolve(stdout);
+      }
     });
+  });
 }
 
 //TODO extract to an helper
-function log(message,color){
-    message = color ? clor[color](message).toString() : message;
-    console.log(message);
+function log(message, color) {
+  message = color ? clor[color](message).toString() : message;
+  console.log(message);
 }
+
 function log2(...params) {
   //if(verbose.LVL>2) {
-    log(params);
+  log(params);
   //}
 }
