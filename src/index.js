838a0ecd2dca07e0adb90b4182c44e2b5f317157
Maxime Warnier <marmax@gmail.com>
2015-10-16 13:40:24 +0200

    style for good linting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Logger from "./logger" ;
+import Logger from "./logger";
 
 
 
@@ -11,57 +11,53 @@ import cli from "./cli";
 
 const handlePluginHelper = require("./handlePluginHelper.js");
 
-  //TODO check input
+//TODO check input
 //console.log(config,cli.args);
 
 //var parsers,processors,reporters;
 
 
-const {config, input} = cli();
+const {
+  config, input
+} = cli();
 
 
 //TODO provide ability to handle multiples parsers
 //TODO parsers could be piped
-handlePlugins(config.parsers,"parser",{})
+handlePlugins(config.parsers, "parser", {})
   //.then((result) => {parsers = result})
   //TODO Fix the name processor > process
-  .then((plugins) => handlePlugins(config.processors,"process",plugins))
-  .then((plugins) => handlePlugins(config.reporters,"reporter",plugins))
+  .then((plugins) => handlePlugins(config.processors, "process", plugins))
+  .then((plugins) => handlePlugins(config.reporters, "reporter", plugins))
   .then((plugins) => {
-      new Codemetrics(input,Logger)
-    .parse(plugins.parser)
-    .process(plugins.processor)
-    .report(plugins.reporter) ;
+    new Codemetrics(input, Logger)
+      .parse(plugins.parser)
+      .process(plugins.processor)
+      .report(plugins.reporter);
   });
 
 
 
-function handlePlugins(configListePlugins,type, plugins){
-    Logger.load(type,Logger.LOG_LVLS.LOADING);
-    return Promise.all(configListePlugins.map((plugin) => handlePluginHelper(plugin,type)))
-  .then((result) => {
-      Logger.load(type,Logger.LOG_LVLS.SUCCESS);
+function handlePlugins(configListePlugins, type, plugins) {
+  Logger.load(type, Logger.LOG_LVLS.LOADING);
+  return Promise.all(configListePlugins.map((plugin) => handlePluginHelper(plugin, type)))
+    .then((result) => {
+      Logger.load(type, Logger.LOG_LVLS.SUCCESS);
 
 
-    //todo rename process > processor
-      if(type === "process") {
-          type="processor";
+      //todo rename process > processor
+      if (type === "process") {
+        type = "processor";
       }
       plugins[type] = result;
       return plugins;
-  })
-  .catch( (result = {msg,errorMsg} ) => {
+    })
+    .catch((result) => {
 
       Logger.error(result.msg);
-      if(result.errorMsg){
-          Logger.bigError(result.errorMsg);
+      if (result.errorMsg) {
+        Logger.bigError(result.errorMsg);
       }
       process.exit(1);
-  });
+    });
 }
-
-
-
-
-
-
